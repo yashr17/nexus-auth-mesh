60180e7c5fa1a71fa03876b93c414efd40d0bb34
yashr17 <yash.reddy.tech@gmail.com>
2026-02-21 20:40:35 +0530

    AUTH-02 - implement jwt signing with RSA encryption

--- nexus-idp/src/main/java/com/nexus/idp/IdpApplication.java
+++ nexus-idp/src/main/java/com/nexus/idp/IdpApplication.java
@@ -2,8 +2,12 @@ package com.nexus.idp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.nexus.idp.config.RsaKeyProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class IdpApplication {
     public static void main(String[] args) {
         SpringApplication.run(IdpApplication.class, args);
