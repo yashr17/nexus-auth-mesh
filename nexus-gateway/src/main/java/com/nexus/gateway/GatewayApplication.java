825367693dd7646d4fb57e0c1b8b76eed4d367ba
yashr17 <yash.reddy.tech@gmail.com>
2026-02-26 21:13:03 +0530

    GATE-1 implement spring cloud gateway

--- nexus-gateway/src/main/java/com/nexus/gateway/GatewayApplication.java
+++ nexus-gateway/src/main/java/com/nexus/gateway/GatewayApplication.java
@@ -3,7 +3,7 @@ package com.nexus.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.nexus")
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
