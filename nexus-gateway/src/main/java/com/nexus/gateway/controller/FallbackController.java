bebaf829a1c65fcf7c48d52f02eb7b45074beb2f
yashr17 <yash.reddy.tech@gmail.com>
2026-03-04 17:08:39 +0530

    GATE-05 Implement Rate Limiting & Circuit Breaking (Resilience4j)

---
+++ nexus-gateway/src/main/java/com/nexus/gateway/controller/FallbackController.java
@@ -0,0 +1,30 @@
+package com.nexus.gateway.controller;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nexus.common.dto.ApiErrorResponse;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackController {
+
+    @RequestMapping("/dummy")
+    public Mono<ResponseEntity<ApiErrorResponse>> dummyServiceFallback() {
+        ApiErrorResponse response = ApiErrorResponse.builder()
+                .timestamp(Instant.now())
+                .status(HttpStatus.SERVICE_UNAVAILABLE.value())
+                .error(HttpStatus.SERVICE_UNAVAILABLE.getReasonPhrase())
+                .message(
+                        "The nexus ecosystem is currently experiencing high load or the target service is temporarily offline")
+                .path(null)
+                .build();
+        return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(response));
+    }
+}
